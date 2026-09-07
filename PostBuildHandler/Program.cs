3457f4fea62c4b06bbddc9b17780c002c27ece59
mgrygo <m.grygo93@gmail.com>
2026-09-07 03:10:37 +0200

    Finished script that copies engine headers to editor build folder on post build event

--- PostBuildHandler/Program.cs
+++ PostBuildHandler/Program.cs
@@ -1,11 +1,70 @@
 ﻿namespace PostBuildHandler
 {
-    // TODO: This will be used for copying public headers to the editor's output directory (for game project generation)
     internal class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello C# World!");
+            if (args.Length != 2)
+            {
+                Console.Error.WriteLine("PostBuildHandler.exe expects following arguments: <Source> <Target>");
+                return;
+            }
+
+            string source = args[0];
+            string target = args[1];
+
+            Console.WriteLine($"PostBuildHandler::Source = {source}");
+            Console.WriteLine($"PostBuildHandler::Target = {target}");
+
+            string includeDirectory = Path.Combine(target, "Include");
+            ClearDirectory(includeDirectory);
+            CopyPublicHeaders(source, includeDirectory);
+
+            Console.WriteLine($"PostBuildHandler::Finished all post build operations");
+        }
+
+        private static void ClearDirectory(string path)
+        {
+            Console.WriteLine($"PostBuildHandler::Clearing path = {path}");
+            if (Directory.Exists(path))
+            {
+                Directory.Delete(path, recursive: true);
+            }
+            Directory.CreateDirectory(path);
+        }
+
+        private static bool CopyPublicHeaders(string source, string target)
+        {
+            bool hasHeaders = false;
+
+            foreach (string file in Directory.GetFiles(source))
+            {
+                if (Path.GetExtension(file).Equals(".h", StringComparison.OrdinalIgnoreCase))
+                {
+                    if (!hasHeaders)
+                    {
+                        Directory.CreateDirectory(target);
+                        hasHeaders = true;
+                    }
+
+                    string fileName = Path.GetFileName(file);
+                    string destination = Path.Combine(target, fileName);
+                    Console.WriteLine($"PostBuildHandler::Copying header from {file} to {destination}");
+                    File.Copy(file, destination, true);
+                }
+            }
+
+            foreach (string directory in Directory.GetDirectories(source))
+            {
+                string directoryName = Path.GetFileName(directory);
+                string targetDirectory = Path.Combine(target, directoryName);
+                if (CopyPublicHeaders(directory, targetDirectory))
+                {
+                    hasHeaders = true;
+                }
+            }
+
+            return hasHeaders;
         }
     }
 }
