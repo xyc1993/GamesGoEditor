677c9b107a06eed18229fc468aaa3e972e47d039
mgrygo <m.grygo93@gmail.com>
2026-09-07 02:17:36 +0200

    Added a PostBuildHandler project to use later for post build events for the editor

---
+++ PostBuildHandler/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace PostBuildHandler
+{
+    // TODO: This will be used for copying public headers to the editor's output directory (for game project generation)
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello C# World!");
+        }
+    }
+}
